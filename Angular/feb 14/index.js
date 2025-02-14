47426e3a1cdd7a87c1c6a21fd04b76fa03d27689
SwapnilPanchade <sspanchade113@gmail.com>
2025-02-14 17:18:42 +0530

    this is commit for task

--- Angular/feb 14/index.js
+++ Angular/feb 14/index.js
@@ -15,3 +15,19 @@ function decrease() {
   if (count > 0) count--;
   ele.innerText = count;
 }
+
+let list = document.getElementById("list").innerHTML;
+function increaseList() {
+  let ul = document.getElementById("list");
+  let newItem = document.createElement("li");
+  ul.appendChild(newItem);
+}
+
+function decreaseList() {
+  let ul = document.getElementById("list");
+  if (ul.children.length > 0) ul.removeChild(ul.lastElementChild);
+}
+
+function resetList() {
+  document.getElementById("list").innerHTML = "li";
+}
